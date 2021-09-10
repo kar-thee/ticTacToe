0092b7210ddf8cc48e60fd2c36627c0b49b4f7a5
kar-thee <ourworldourpeople@gmail.com>
2021-09-10 07:21:28 +0530

    Styled the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Board from "./components/Board";
 
+import "./styles/style.scss";
+
   const App = () => {
   return(
-    <>
+    <div className="app">
     <h1>Tic-Tac-Toe</h1>
       <Board />
-    </>
+    </div>
   );
 }
 export default App;
